b0e025907d63f1520d5bb01d54ced69c496538de
Tuvao <s305524@student.hioa.no>
2017-10-17 18:41:33 +0200

    Kopierer den inn

--- Tusimaka/DAL/AdminDAL.cs
+++ Tusimaka/DAL/AdminDAL.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace Tusimaka.DAL
 {
     public class AdminDAL
-    {      
+    {
+
+        private static bool Bruker_i_DB(bruker innBruker)
+        {
+            using (var db = new BrukerContext())
+            {
+                byte[] passordDb = lagHash(innBruker.Passord);
+                dbBruker funnetBruker = db.Brukere.FirstOrDefault
+                (b => b.Passord == passordDb && b.Navn == innBruker.Navn);
+                if (funnetBruker == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
     }
 }
