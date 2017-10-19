f81c3044fd7f835fa4612af36feaa4bfa679f386
Silje Foss Olsen <s305511@stud.hioa.no>
2017-10-19 03:00:22 +0200

    Bare tull - ikke kommet noe steder

--- Tusimaka/BLL/AdminBestillingBLL.cs
+++ Tusimaka/BLL/AdminBestillingBLL.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tusimaka.DAL;
+using Tusimaka.Model;
 
 namespace Tusimaka.BLL
 {
     public class AdminBestillingBLL
     {
+        
     }
 }
--- Tusimaka/DAL/AdminBestillingDAL.cs
+++ Tusimaka/DAL/AdminBestillingDAL.cs
@@ -9,36 +9,104 @@ namespace Tusimaka.DAL
 {
     public class AdminBestillingDAL
     {
-        public List<strekning> hentAlleFlyBestillinger(int KundeId)
-        {
-            using (var db = new DBContext())
-            {
-                //List<strekning> strekninger = new List<strekning>();
-                //innparameter: kundeID
-                //Finn Flybestilling knyttet til kunde i Flybestillingkunde. 
-                //hent ut flybestillingsID fra flybestillingkunde
-                //List flybestilling where flybestillingsID = flybestID
-                List<FlyBestillingKunder> kundeSineBestillinger = db.FlyBestillingKunder.Where(f => f.kundeID == KundeId).ToList();
-                foreach (FlyBestillingKunder bestillinger in kundeSineBestillinger)
-                {
-                    List<FlyBestilling> flybestillingsId = db.FlyBestilling.Where(f => f.flyBestillingsID == bestillinger.flyBestillingsID).ToList();
+        //TEST3
+        //public List<strekning> test(int KundeId)
+        //{
+        //    var db = new DBContext();
 
-                    foreach (FlyBestilling flybestillinger in flybestillingsId)
-                    {
-                        List<strekning> strekninger = db.Strekning.Where(s => s.StrekningsID == flybestillinger.StrekningsID).Select(s => new strekning()
-                        {
-                            fraFlyplass = s.fraFlyplass,
-                            tilFlyplass = s.tilFlyplass,
-                            dato = s.dato,
-                            tid = s.tid,
-                            pris = s.pris,
-                            flyTid = s.flyTid,
-                            antallLedigeSeter = s.antallLedigeSeter
-                            }).ToList();
-                    }
-                }
-                return strekninger;
-            }
-        }
+        //    var enKunde = db.Kunder.Find(KundeId);
+
+        //    if (enKunde == null)
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        var utKunde = new Kunde()
+        //        {
+        //            id = enDbKunde.ID,
+        //            fornavn = enDbKunde.Fornavn,
+        //            etternavn = enDbKunde.Etternavn,
+        //            adresse = enDbKunde.Adresse,
+        //            postnr = enDbKunde.Postnr,
+        //            poststed = enDbKunde.Poststeder.Poststed
+        //        };
+        //        return utKunde;
+        //    }
+
+        //Tankegang
+        //public List<strekning> hentKundesFlyBestillingsID(int KundeId)
+        //{
+        //    using (var db = new DBContext())
+        //    {
+
+        //        //List<strekning> strekninger = new List<strekning>();
+        //        //innparameter: kundeID
+        //        //Finn Flybestilling knyttet til kunde i Flybestillingkunde. 
+        //        //hent ut flybestillingsID fra flybestillingkunde
+        //        //List flybestilling where flybestillingsID = flybestID
+
+        //        //TEST1
+        //        //List<FlyBestillingKunder> kundeSineBestillinger = db.FlyBestillingKunder.Where(f => f.kundeID == KundeId).ToList();
+        //        //foreach(FlyBestillingKunder bestillinger in kundeSineBestillinger)
+        //        //{
+        //        //    List<FlyBestilling> flybestillingsId = db.FlyBestilling.Where(f => f.flyBestillingsID == bestillinger.flyBestillingsID).Select(f => new FlyBestilling()
+        //        //    {
+        //        //        flyBestillingsID = f.flyBestillingsID,
+        //        //        StrekningsID = f.StrekningsID
+        //        //    }).ToList();
+        //        //    foreach (FlyBestilling flybestillinger in flybestillingsId)
+        //        //    {
+        //        //        var denneKunden = db.FlyBestilling.Find(flybestillingsId);
+
+        //        //        if (denneKunden == null)
+        //        //        {
+        //        //            return null;
+        //        //        }
+        //        //        else
+        //        //        {
+        //        //            List<strekning> utBestillinger = db.Strekning.Where(s => s.StrekningsID == denneKunden.StrekningsID).Select(s => new strekning()
+        //        //            {
+        //        //                fraFlyplass = s.fraFlyplass,
+        //        //                tilFlyplass = s.tilFlyplass,
+        //        //                dato = s.dato,
+        //        //                tid = s.tid,
+        //        //                pris = s.pris,
+        //        //                flyTid = s.flyTid,
+        //        //                antallLedigeSeter = s.antallLedigeSeter
+        //        //            }).ToList();
+        //        //        }
+        //        //    }
+
+        //        //}
+        //        //return utBestillinger.ToList();
+
+
+        //        //TEST2
+        //        //List<FlyBestillingKunder> kundeSineBestillinger = db.FlyBestillingKunder.Where(f => f.kundeID == KundeId).ToList();
+
+        //        //foreach (FlyBestillingKunder bestillinger in kundeSineBestillinger)
+        //        //{
+
+        //        //    List<FlyBestilling> flybestillingsId = db.FlyBestilling.Where(f => f.flyBestillingsID == bestillinger.flyBestillingsID).ToList();
+
+        //        //    foreach (FlyBestilling flybestillinger in flybestillingsId)
+        //        //    {
+        //        //        List<strekning> strekninger = db.Strekning.Where(s => s.StrekningsID == flybestillinger.StrekningsID).Select(s => new strekning()
+        //        //        {
+        //        //            fraFlyplass = s.fraFlyplass,
+        //        //            tilFlyplass = s.tilFlyplass,
+        //        //            dato = s.dato,
+        //        //            tid = s.tid,
+        //        //            pris = s.pris,
+        //        //            flyTid = s.flyTid,
+        //        //            antallLedigeSeter = s.antallLedigeSeter
+        //        //        }).ToList();
+        //        //    }
+        //        //}
+        //        //return strekninger.ToList();
+               
+        //    }
+        //}
     }
 }
