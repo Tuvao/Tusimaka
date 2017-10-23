b8c07bfe2825aec12c9684edeaca4288323c39b5
Karianne <Karianne@DESKTOP-OR4S2CS>
2017-10-23 14:49:24 +0200

    Jobber med enhetstesting. Har lagt til klasser i DAL og BLL

---
+++ Tusimaka/Enhetstest/AdminControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Enhetstest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Liste()
+        {
+            //denne tester kun på en metode som kun returnerer et view
+            // Arrange
+            var controller = new KundeController(new KundeLogikk(new KundeRepositoryStub()));
+            // Act
+            var resultat = (ViewResult)controller.Registrer();
+            // Assert
+            Assert.AreEqual(resultat.ViewName, "");
+
+            public ActionResult RegistrerKunde()
+            {
+                return View();
+            }
+            return View();
+            }
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
