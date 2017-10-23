beeb068d11683c6af9deb7f4c9e8528aa80202bb
Silje Foss Olsen <s305511@stud.hioa.no>
2017-10-24 01:43:43 +0200

    Stub til FlyRuter er delvis OK

--- Tusimaka/DAL/AdminFlyruterDALRepositoryStub.cs
+++ Tusimaka/DAL/AdminFlyruterDALRepositoryStub.cs
@@ -9,5 +9,30 @@ namespace Tusimaka.DAL
 {
     public class AdminFlyruterDALRepositoryStub : DAL.IAdminFlyruterRepository
     {
+        public List<Strekning> hentAlleFlyruter()
+        {
+            var flyListe = new List<Strekning>();
+            var flyrute1 = new Strekning()
+            {
+                
+            }
+
+        }
+        public bool lagreFlyrute(Strekning innFlyrute)
+        {
+
+        }
+        public bool slettFlyrute(int id)
+        {
+
+        }
+        public bool endreFlyrute(int id, Strekning innFlyrute)
+        {
+
+        }
+        public Strekning hentDenneFlyruten(int id)
+        {
+
+        }
     }
 }
