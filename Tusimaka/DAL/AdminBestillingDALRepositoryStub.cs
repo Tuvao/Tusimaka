bbc939a57ea800f0a14c0a045b1b1cbcdd83a905
DESKTOP-BOR2DVL\maria <maria.overlier.berg96@gmail.com>
2017-10-24 10:30:05 +0200

    AdminFlyruterDALRepositoryStub og AdminBestillingDALRepositoryStub

--- Tusimaka/DAL/AdminBestillingDALRepositoryStub.cs
+++ Tusimaka/DAL/AdminBestillingDALRepositoryStub.cs
@@ -9,5 +9,63 @@ namespace Tusimaka.DAL
 {
     public class AdminBestillingDALRepositoryStub : DAL.IAdminBestillingRepository
     {
+        public bool lagreFlyBestilling(FlyBestillinger innFlyinfo)
+        {
+            if (innFlyinfo.AntallPersoner == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool lagreKundeIdMotFlyBestilling(Kunde innKunde, FlyBestillinger innFlyinfo)
+        {
+
+        }
+        public string hentAntallPersoner(int id)
+        {
+            if (id == 0)
+            {
+                var bestilling = new FlyBestillinger();
+                bestilling.AntallPersoner = 0;
+                return bestilling.AntallPersoner.ToString();
+            }
+            else
+            {
+                var bestilling = new FlyBestillinger()
+                {
+                    FlyBestillingsID = 1,
+                    StrekningsID = 1,
+                    AntallPersoner = 2,
+                    ReturID = 3,
+                };
+                return bestilling.AntallPersoner.ToString();
+            }
+        }
+        public string hentBestilling(int id)
+        {
+            if (id == 0)
+            {
+                var bestilling = new FlyBestillinger();
+                bestilling.FlyBestillingsID = 0;
+                return bestilling.ToString();
+            }
+            else
+            {
+                var bestilling = new FlyBestillinger()
+                {
+                    FlyBestillingsID = 1,
+                    StrekningsID = 1,
+                    AntallPersoner = 2,
+                    ReturID = 3,
+                };
+                return bestilling.ToString();
+            }
+        }
+        public string hentReferanseNR()
+        {
+
+        }
     }
-}
--- Tusimaka/DAL/AdminFlyruterDALRepositoryStub.cs
+++ Tusimaka/DAL/AdminFlyruterDALRepositoryStub.cs
@@ -14,25 +14,76 @@ namespace Tusimaka.DAL
             var flyListe = new List<Strekning>();
             var flyrute1 = new Strekning()
             {
-                
-            }
-
+                StrekningsID = 1,
+                FraFlyplass = "Bergen",
+                TilFlyplass = "Oslo",
+                Dato = "2017-10-20",
+                Tid = "12:30",
+                Pris = 1234,
+                FlyTid = 45,
+                AntallLedigeSeter = 4
+            };
+            flyListe.Add(flyrute1);
+            flyListe.Add(flyrute1);
+            flyListe.Add(flyrute1);
+            return flyListe;
         }
         public bool lagreFlyrute(Strekning innFlyrute)
         {
-
+            if(innFlyrute.FraFlyplass == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         public bool slettFlyrute(int id)
         {
-
+            if (id == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         public bool endreFlyrute(int id, Strekning innFlyrute)
         {
-
+            if (id == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
         public Strekning hentDenneFlyruten(int id)
         {
-
+            if (id == 0)
+            {
+                var flyrute = new Strekning();
+                flyrute.StrekningsID = 0;
+                return flyrute;
+            }
+            else
+            {
+                var flyrute = new Strekning()
+                {
+                    StrekningsID = 1,
+                    FraFlyplass = "Bergen",
+                    TilFlyplass = "Oslo",
+                    Dato = "2017-10-20",
+                    Tid = "12:30",
+                    Pris = 1234,
+                    FlyTid = 45,
+                    AntallLedigeSeter = 4
+                };
+                return flyrute;
+            }
         }
     }
 }
