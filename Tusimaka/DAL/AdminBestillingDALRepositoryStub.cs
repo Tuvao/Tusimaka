cfa8ed478f9e73547cc49810cc6ba32a8f797bbc
DESKTOP-BOR2DVL\maria <maria.overlier.berg96@gmail.com>
2017-10-24 11:25:06 +0200

    fixer stubs

--- Tusimaka/DAL/AdminBestillingDALRepositoryStub.cs
+++ Tusimaka/DAL/AdminBestillingDALRepositoryStub.cs
@@ -9,94 +9,22 @@ namespace Tusimaka.DAL
 {
     public class AdminBestillingDALRepositoryStub : DAL.IAdminBestillingRepository
     {
-        public bool lagreFlyBestilling(FlyBestillinger innFlyinfo)
+
+        public bool endreKunde(int id, Kunde innKunde)
         {
-            if (innFlyinfo.AntallPersoner == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+
         }
-        public bool lagreKundeIdMotFlyBestilling(Kunde innKunde, FlyBestillinger innFlyinfo)
+        public List<KundeBestillinger> hentKundesFlyBestillinger(int id)
         {
-            if (innKunde.KundeID == 0)
-            {
-                return false;
-            }
-            else
-            {
-                if (innFlyinfo.AntallPersoner == 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
+
         }
-        public string hentAntallPersoner(int id)
+        public bool LagreAdminFlyBestilling(int id, FlyBestillinger nyBestilling)
         {
-            if (id == 0)
-            {
-                var bestilling = new FlyBestillinger();
-                bestilling.AntallPersoner = 0;
-                return bestilling.AntallPersoner.ToString();
-            }
-            else
-            {
-                var bestilling = new FlyBestillinger()
-                {
-                    FlyBestillingsID = 1,
-                    StrekningsID = 1,
-                    AntallPersoner = 2,
-                    ReturID = 3,
-                };
-                return bestilling.AntallPersoner.ToString();
-            }
+
         }
-        public string hentBestilling(int id)
+        public bool SlettKundeBestilling(int id)
         {
-            if (id == 0)
-            {
-                var bestilling = new FlyBestillinger();
-                bestilling.FlyBestillingsID = 0;
-                return bestilling.ToString();
-            }
-            else
-            {
-                var bestilling = new FlyBestillinger()
-                {
-                    FlyBestillingsID = 1,
-                    StrekningsID = 1,
-                    AntallPersoner = 2,
-                    ReturID = 3,
-                };
-                return bestilling.ToString();
-            }
-        }
-        public string hentReferanseNR(int id)
-        {
-            if (id == 0)
-            {
-                var bestilling = new FlyBestillinger();
-                bestilling.FlyBestillingsID = 0;
-                return bestilling.FlyBestillingsID.ToString();
-            }
-            else
-            {
-                var bestilling = new FlyBestillinger()
-                {
-                    FlyBestillingsID = 1,
-                    StrekningsID = 1,
-                    AntallPersoner = 2,
-                    ReturID = 3,
-                };
-                return bestilling.FlyBestillingsID.ToString();
-            }
+
         }
     }
 }
